9084b6ae17c76bbc7b6836ac547ce7a07d55a47d
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-01-14 23:22:10 -0500

    POST and GET all posts works

--- routers/post.js
+++ routers/post.js
@@ -9,7 +9,39 @@ router.use('/:id/comments', commentRouter);
 // === END POINTS === //
 
 // POST - /api/posts
+router.post('/', (req, res) => {
+  if(!req.body.title || !req.body.contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    })
+  }
+
+  db.insert(req.body)
+    .then(data => {
+      res.status(201).json(data)
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        errorMessage: "There was an error while saving the post to the database."
+      });
+    })
+});
+
 // GET - /api/posts
+router.get("/", (req, res) => {
+  db.find(req.body)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        errorMessage: "The posts information could not be retrieved."
+      })
+    })
+});
+
 // GET - /api/posts/:id
 // DELETE - /api/posts/:id
 // PUT - /api/posts/:id
