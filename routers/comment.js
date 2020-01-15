e21e1e8fc85ca397570db89979f3da502514f70c
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-01-15 12:16:19 -0500

    POST is working, but I can't GET the new Comment

--- routers/comment.js
+++ routers/comment.js
@@ -8,35 +8,37 @@ const router = express.Router({
 // === END POINTS === //
 
 // POST - /api/posts/:id/comments
-// Keep receiving a 500 error
+// POST is working, but I can't GET the new Comment
 router.post("/", (req, res) => {
-  if(!req.body.text) {
-    return res.status(400).json({
-      errorMessage: "Please provide text for the comment."
-    })
+  const newComment = req.body;
+  db.insertComment(newComment);
+
+  const ID = req.params.id;
+
+  if (newComment.length === 0) {
+    res.status(404).json({
+      message: "The post with the specific ID does not exist"
+    });
   }
 
-  const payload = {
-    text: req.body.text,
-  }
-
-  db.insertComment(req.params.id, payload)
-    .then(data => {
-      if(data) {
-        res.status(201).json(data);
+  db.findById(ID)
+    .then(newCom => {
+       if (!newCom) {
+        res.status(400).json({
+          errorMessage: "Please provide text for the comment"
+        });
       } else {
-        res.status(404).json({
-          errorMessage: "The post with the specified ID does not exist."
-        })
+        res.status(201).json({ newCom });
       }
     })
     .catch(err => {
-      console.error(err);
+      console.log(err);
       res.status(500).json({
-        errorMessage: "There was an error while saving the comment to the database."
-      })
-    })
+        errorMessage: "There was an error while saving the post to the database"
+      });
+    });
 });
+
 
 // GET - /api/posts/:id/comments
 router.get("/", (req, res) => {
