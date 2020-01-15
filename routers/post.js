1bc555718ede818d220ceaa0ea159467a45ae7f4
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-01-14 23:35:09 -0500

    Added GET for post ID. Need to fix id error.

--- routers/post.js
+++ routers/post.js
@@ -43,6 +43,26 @@ router.get("/", (req, res) => {
 });
 
 // GET - /api/posts/:id
+// Will not show error messages with invalid id parameter
+router.get("/:id", (req, res) => {
+  db.findById(req.params.id)
+    .then(data => {
+      if(data) {
+        res.status(200).json(data);
+      } else {
+        res.status(404).json({
+          errorMessage: "The post with the specified ID does not exist."
+        })
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+          errorMessage: "The post information could not be retrieved."
+        })
+    })
+})
+
 // DELETE - /api/posts/:id
 // PUT - /api/posts/:id
 
