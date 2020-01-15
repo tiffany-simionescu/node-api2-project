6c1e164ee18c1238d995c658bb7c337c595acf3c
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-01-14 23:50:08 -0500

    PUT works

--- routers/post.js
+++ routers/post.js
@@ -86,5 +86,30 @@ router.delete("/:id", (req, res) => {
 });
 
 // PUT - /api/posts/:id
+router.put("/:id", (req, res) => {
+  const changes = req.body;
+  if(!req.body.title || !req.body.contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    })
+  }
+
+  db.update(req.params.id, changes)
+    .then(data => {
+      if(data) {
+        res.status(200).json(data);
+      } else {
+        res.status(404).json({
+          errorMessage: "The post with the specified ID does not exist."
+        })
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        errorMessage: "The post information could not be modified."
+      })
+    })
+});
 
 module.exports = router;
